dbbdbd39afe0cfeb60acc42ebe2b0253663ce590
Fidelis <yonlifidelis2@gmail.com>
2023-09-07 16:05:06 +0200

    MàJ

--- unitTestsDW.sql
+++ unitTestsDW.sql
@@ -3,12 +3,15 @@
 --Price >0
 --Weight >0
 --Email non missingValues
+--TotalAmount >0
+
 
 --Capacity >0
 --QualityScore non missingValues
---TotalAmount >0
 --StockeLevel >0
 --RestockThreshold >0
+
+
 
 
 
@@ -17,9 +20,13 @@
 SELECT SCHEMA_NAME
 FROM INFORMATION_SCHEMA.SCHEMATA
 WHERE SCHEMA_NAME = 'tSQLt';
------Liste des objects (tests) contenus dans tSQLt
+-----Nombre d'objets (tests) contenus dans tSQLt
 SELECT *
 FROM tSQLt.TestClasses;
+
+
+
+
 
 
 USE Sales_Olap_Loading_DW;
@@ -27,7 +34,10 @@ USE Sales_Olap_Loading_DW;
 ----Création du schéma
 EXEC tSQLt.NewTestClass 'Sales_Olap_Loading_DW_Tests';
 
-----Création des tests
+
+
+
+---------Création des tests
 
 --TestAgesOver13
 CREATE PROCEDURE Sales_Olap_Loading_DW_Tests.[testAgesOver13] 
@@ -45,6 +55,8 @@ BEGIN
 END;
 
 
+
+
 --TestPrice
 CREATE PROCEDURE Sales_Olap_Loading_DW_Tests.[TestPrice] 
 AS
@@ -59,6 +71,7 @@ BEGIN
     -- Assert (Vérification) : Vérifiez que le nombre de valeurs négatives est égal à zéro
     EXEC tSQLt.AssertEquals 0, @NegativePrices;
 END;
+
 
 
 
@@ -79,21 +92,130 @@ END;
 
 
 
---TestMissingEmails
-CREATE PROCEDURE Sales_Olap_Loading_DW_Tests.[TestWeight] 
+
+-- TestMissingEmails
+CREATE PROCEDURE Sales_Olap_Loading_DW_Tests.[TestMissingEmails] 
 AS
 BEGIN
-    -- Act (Action) : Vérifiez si toutes les valeurs de la colonne "Weight" sont positives
-    DECLARE @NegativeWeights INT;
-	
-    SELECT @NegativeWeights = COUNT(*)
-    FROM DimProduct
-    WHERE Weight < 0;
+    -- Act (Action) : Sélectionnez toutes les valeurs de la colonne "Email" où le champ est NULL
+    DECLARE @MissingEmails INT;
 
-    -- Assert (Vérification) : Vérifiez que le nombre de valeurs négatives est égal à zéro
-    EXEC tSQLt.AssertEquals 0, @NegativeWeights;
+    SELECT @MissingEmails = COUNT(*)
+    FROM DimCustomer
+    WHERE Email IS NULL;
+
+    -- Assert (Vérification) : Vérifiez que le nombre de valeurs manquantes est égal à zéro
+    EXEC tSQLt.AssertEquals 0, @MissingEmails;
 END;
 
 
 
+
+--TestTotalAmount 
+CREATE PROCEDURE Sales_Olap_Loading_DW_Tests.[TestTotalAmount] 
+AS
+BEGIN
+    -- Act (Action) : Vérifiez si toutes les valeurs de la colonne "Capacity" sont positives
+    DECLARE @NegativeTotalAmount INT;
+	
+    SELECT @NegativeTotalAmount = COUNT(*)
+    FROM FactOrder
+    WHERE TotalAmount < 0;
+
+    -- Assert (Vérification) : Vérifiez que le nombre de valeurs négatives est égal à zéro
+    EXEC tSQLt.AssertEquals 0, @NegativeTotalAmount;
+END;
+
+
+
+
+--TestCapacity
+CREATE PROCEDURE Sales_Olap_Loading_DW_Tests.[TestCapacity] 
+AS
+BEGIN
+    -- Act (Action) : Vérifiez si toutes les valeurs de la colonne "Capacity" sont positives
+    DECLARE @NegativeCapacities INT;
+	
+    SELECT @NegativeCapacities = COUNT(*)
+    FROM DimWarehouse
+    WHERE Capacity < 0;
+
+    -- Assert (Vérification) : Vérifiez que le nombre de valeurs négatives est égal à zéro
+    EXEC tSQLt.AssertEquals 0, @NegativeCapacities;
+END;
+
+
+
+--TestMissingQualityScore
+CREATE PROCEDURE Sales_Olap_Loading_DW_Tests.[TestMissingQualityScore] 
+AS
+BEGIN
+    -- Act (Action) : Sélectionnez toutes les valeurs de la colonne "QualityScore" où le champ est NULL
+    DECLARE @MissingQualityScore INT;
+
+    SELECT @MissingQualityScore = COUNT(*)
+    FROM DimSupplier
+    WHERE QualityScore IS NULL;
+
+    -- Assert (Vérification) : Vérifiez que le nombre de valeurs manquantes est égal à zéro
+    EXEC tSQLt.AssertEquals 0, @MissingQualityScore;
+END;
+
+
+
+
+--TestStockLevel
+CREATE PROCEDURE Sales_Olap_Loading_DW_Tests.[TestStockLevel] 
+AS
+BEGIN
+    -- Act (Action) : Vérifiez si toutes les valeurs de la colonne "Capacity" sont positives
+    DECLARE @NegativeStockLevel INT;
+	
+    SELECT @NegativeStockLevel = COUNT(*)
+    FROM FactInventory
+    WHERE StockLevel < 0;
+
+    -- Assert (Vérification) : Vérifiez que le nombre de valeurs négatives est égal à zéro
+    EXEC tSQLt.AssertEquals 0, @NegativeStockLevel;
+END;
+
+
+
+--TestRestockThreshold
+CREATE PROCEDURE Sales_Olap_Loading_DW_Tests.[TestRestockThreshold] 
+AS
+BEGIN
+    -- Act (Action) : Vérifiez si toutes les valeurs de la colonne "Capacity" sont positives
+    DECLARE @NegativeRestockThreshold INT;
+	
+    SELECT @NegativeRestockThreshold = COUNT(*)
+    FROM DimRestockThreshold
+    WHERE RestockThreshold < 0;
+
+    -- Assert (Vérification) : Vérifiez que le nombre de valeurs négatives est égal à zéro
+    EXEC tSQLt.AssertEquals 0, @NegativeRestockThreshold;
+END;
+
+
+
+
+--Exécution de de chaque test
+EXEC tSQLt.Run 'Sales_Olap_Loading_DW_Tests.[TestAgesOver13]' ;
+EXEC tSQLt.Run 'Sales_Olap_Loading_DW_Tests.[TestPrice]' ;
+EXEC tSQLt.Run 'Sales_Olap_Loading_DW_Tests.[TestWeight]' ;
+EXEC tSQLt.Run 'Sales_Olap_Loading_DW_Tests.[TestMissingEmails]' ;
+EXEC tSQLt.Run 'Sales_Olap_Loading_DW_Tests.[TestTotalAmountTestTotalAmount]' ;
+
+EXEC tSQLt.Run 'Sales_Olap_Loading_DW_Tests.[TestCapacity]' ;
+EXEC tSQLt.Run 'Sales_Olap_Loading_DW_Tests.[TestMissingQualityScore]' ;
+EXEC tSQLt.Run 'Sales_Olap_Loading_DW_Tests.[TestRestockThreshold]' ;
+EXEC tSQLt.Run 'Sales_Olap_Loading_DW_Tests.[TestStockLevel]' ;
+
+
+
+
+
+
+--Exécution de tous les tests
+EXEC tSQLt.RunAll;
 
